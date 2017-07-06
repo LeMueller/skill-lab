4b796dd3549105f76e03fbd3a3e00c7a9a177569
System <none>
2017-07-06 16:35:10 +0000

    clock finish

---
+++ js/scaleonmouseenter.js
@@ -0,0 +1,12 @@
+AFRAME.registerComponent('scale-on-mouseenter', {
+    schema: {
+      to: {default: '2.5 2.5 2.5'}
+    },
+
+    init: function () {
+      var data = this.data;
+      this.el.addEventListener('mouseenter', function () {
+        this.setAttribute('scale', data.to);
+      });
+    }
+});
