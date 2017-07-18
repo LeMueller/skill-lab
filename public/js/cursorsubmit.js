57f19845eca57e634bf39accf2793fcadd9da716
System <none>
2017-07-18 09:58:51 +0000

    cupboard finish, cabinet almost finish

--- public/js/cursorsubmit.js
+++ public/js/cursorsubmit.js
@@ -1,15 +1,16 @@
 AFRAME.registerComponent('cursor-submit', {
   init: function () {
-    var _this=this;
+    var _this=this.el;
     this.el.addEventListener('click', function (evt) {
-      this.setAttribute('material', 'color', 'red');
+      //var _this=this;
+      _this.setAttribute('material', 'color', 'red');
       //_this.setAttribute('material', 'color', 'green'); //_this.setAttribute is not a function
       //this.setAttribute('material', 'color', 'green'); //works well
       setTimeout(function(){
         //alert(_this); //works well
         //this.setAttribute('material', 'color', 'green');  //this.setAttribute is not a function
-        //_this.setAttribute('material', 'color', 'green'); //_this.setAttribute is not a function
-      },2000);
+        _this.setAttribute('material', 'color', 'black'); //_this.setAttribute is not a function
+      },300);
     });
   }
 });
