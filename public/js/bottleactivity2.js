31ba5d964efcbce5c141deaed6e2e9840b85f6d9
System <none>
2017-08-01 14:52:25 +0000

    bottle animation finish

--- public/js/bottleactivity2.js
+++ public/js/bottleactivity2.js
@@ -27,7 +27,7 @@ AFRAME.registerComponent('bottleactivity2',{
       }else if(status === 4){
         takeoffcap(el);
       }else if(status === 5){
-        this.hang
+        hang(el);
       }
       if(status<=6){
          status++;
@@ -105,32 +105,95 @@ function movetodesk(el){
 
 function takeoffcap(el){
   var oldMove = el.getElementsByTagName("a-animation");
+  //alert(oldMove[0]);
   el.removeChild(oldMove[0]);
-  el.removeChild(oldMove[1]);
+  el.removeChild(oldMove[0]);
   //alert("take off cap");
 
   var cap = el.querySelector('#bottlecap');
-  alert("cap before remove: "+cap);
+  //alert("cap before remove: "+cap);
+  el.removeChild(cap);
+  //alert("cap after remove: "+cap);  
+  var scene = document.querySelector('#scene');
+  var abwurfdecker = scene.querySelector("#abwurfmodelcapnewpivot");
+  //alert(scene);
+  scene.appendChild(cap);
   cap.setAttribute("position", "-0.27 0.826 -0.83");
   cap.setAttribute("rotation", "0 90 0 ");
   cap.setAttribute("scale", "0.1 0.1 0.1");
-  el.removeChild(cap);
-  alert("cap after remove: "+cap);
-  var scene = document.querySelector('#scene');
-  //alert(scene);
-  scene.appendChild(cap);
-
+  
   var move = document.createElement("a-animation");
   move.setAttribute("begin","0");
-  move.setAttribute("attribute", "positon");
-  move.setAttribute("to", '-0.27 1 -0.83');
+  move.setAttribute("attribute", "position");
+  move.setAttribute("to", '-0.27 0.9 -0.83');
   move.setAttribute("dur", 300);
-
   cap.appendChild(move);
+  
+  var t1 = setTimeout(function(){
+    var move2 = document.createElement("a-animation");
+    move2.setAttribute("begin","0");
+    move2.setAttribute("attribute", "position");
+    move2.setAttribute("to", '-0.01 0.9 -0.9');
+    move2.setAttribute("dur", 300);
+    cap.appendChild(move2);
+    
+    
+    //alert(abwurfdecker);
+    var deckermove = document.createElement("a-animation");
+    deckermove.setAttribute("begin","0");
+    deckermove.setAttribute("attribute", "rotation");
+    deckermove.setAttribute("to", '0 0 -45');
+    deckermove.setAttribute("dur", 300);
+    abwurfdecker.appendChild(deckermove);
+  },300);
+  
+  var t2 = setTimeout(function(){
+    var move3 = document.createElement("a-animation");
+    move3.setAttribute("begin","0");
+    move3.setAttribute("attribute", "position");
+    move3.setAttribute("to", '-0.01 0.73 -0.9');
+    move3.setAttribute("dur", 300);
+    cap.appendChild(move3);
+  },600);
+  
+  var t3 = setTimeout(function(){
+    var deckermovecopy = abwurfdecker.querySelector("a-animation");
+    abwurfdecker.removeChild(deckermovecopy);
+    var deckermove2 = document.createElement("a-animation");
+    deckermove2.setAttribute("begin","0");
+    deckermove2.setAttribute("attribute", "rotation");
+    deckermove2.setAttribute("to", '0 0 0');
+    deckermove2.setAttribute("dur", 300);
+    abwurfdecker.appendChild(deckermove2);
+  },800);
+  
+  var t4 = setTimeout(function(){
+    var deckermovecopy = abwurfdecker.querySelector("a-animation");
+    abwurfdecker.removeChild(deckermovecopy);
+  },900);
 }
 
 function hang(el){    
   //alert("hang");
-  el.setAttribute("position", "1.156 1.513 -0.63");
-  el.setAttribute("rotation", "180 270 0");
+
+  var move = document.createElement("a-animation");
+  move.setAttribute("begin","0");
+  move.setAttribute("attribute", "rotation");
+  move.setAttribute("to", '180 270 0');
+  move.setAttribute("dur", 500);
+
+  var move2 = document.createElement("a-animation");
+  move2.setAttribute("begin","0");
+  move2.setAttribute("attribute", "position");
+  move2.setAttribute("to", '1.156 1.513 -0.63');
+  move2.setAttribute("dur", 500);
+
+  el.appendChild(move);
+  el.appendChild(move2);
+  
+  
+  var t = setTimeout(function(){
+    el.removeChild(move);
+    el.removeChild(move2);
+  },900);
 }
