c805d836c95711a4fd6c7def6ce7af905523d970
Glitch (skill-lab) <none>
2017-10-17 15:39:06 +0000

    animation of desinfection cloth finish

---
+++ public/js/takedesinfectioncloth.js
@@ -0,0 +1,78 @@
+AFRAME.registerComponent('takedesinfectioncloth',{
+  schema:{
+    
+    message:{type:"string", default:"takedesinfectioncloth"},
+    position:{default:'0 0.55 -0.05'},
+  },
+
+  
+  init:function(){
+    var self=this;
+    var data= this.data;
+    var el=this.el;
+
+    var status=0;
+
+    el.addEventListener('click', function(){
+ 
+    });
+
+  },
+});
+
+
+//desk desinfection
+var move = document.createElement("a-animation");
+move.setAttribute("begin","0");
+move.setAttribute("attribute", "position");
+move.setAttribute("to", '-0.39 0.679 -0.81');
+move.setAttribute("dur", 500);
+move.setAttribute("direction","alternate");
+move.setAttribute("repeat",3);
+move.setAttribute("easing","ease");
+
+
+
+function desinfection(el){
+  if(!el.hasChildNodes()){
+    el.appendChild(move);
+  }
+
+  var t= setTimeout(function(){
+    var oldMove = el.getElementsByTagName("a-animation");
+    el.removeChild(oldMove[0]);
+
+  },2000);
+}
+
+//throw into ashcan
+var move21 = document.createElement("a-animation");
+move21.setAttribute("begin","0");
+move21.setAttribute("attribute", "position");
+move21.setAttribute("to", '-0.89 0.679 -0.81');
+move21.setAttribute("dur", 500);
+
+var move22 = document.createElement("a-animation");
+move22.setAttribute("begin","0");
+move22.setAttribute("attribute", "position");
+move22.setAttribute("to", '-0.89 0.1 -0.81');
+move22.setAttribute("dur", 500);
+
+function moveToAshcan(el){
+  
+  if(!el.hasChildNodes()){
+    el.appendChild(move21);
+    
+    var t= setTimeout(function(){
+      var oldMove = el.getElementsByTagName("a-animation");
+      el.removeChild(oldMove[0]);
+      el.appendChild(move22);
+    },500);
+    
+    var t2= setTimeout(function(){
+      var oldMove = el.getElementsByTagName("a-animation");
+      el.removeChild(oldMove[0]);
+    },1000);
+  }
+  
+}
